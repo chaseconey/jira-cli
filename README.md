d4d8b526f15628ac203ffa90f83d2161d84bcd69
Chase Coney <chasiepoo@gmail.com>
2015-07-01 14:55:30 -0500

    Switching to psr2 code standards.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## jira-cli
 
+[![StyleCI](https://styleci.io/repos/35121919/shield)](https://styleci.io/repos/35121919/)
 [![Latest Stable Version](https://poser.pugx.org/chaseconey/jira-cli/v/stable)](https://packagist.org/packages/chaseconey/jira-cli) [![Total Downloads](https://poser.pugx.org/chaseconey/jira-cli/downloads)](https://packagist.org/packages/chaseconey/jira-cli) [![Latest Unstable Version](https://poser.pugx.org/chaseconey/jira-cli/v/unstable)](https://packagist.org/packages/chaseconey/jira-cli) [![License](https://poser.pugx.org/chaseconey/jira-cli/license)](https://packagist.org/packages/chaseconey/jira-cli)
 
 A very simple command-line interface to your Jira instance.
--- src/SearchCommand.php
+++ src/SearchCommand.php
@@ -11,63 +11,68 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SearchCommand extends Command {
+class SearchCommand extends Command
+{
 
-	public function configure() {
-		$this->setName('search')
-			->setDescription('Search for an issue in Jira')
-			->addArgument('search', InputArgument::REQUIRED, 'Search term');
-	}
+    public function configure()
+    {
+        $this->setName('search')
+            ->setDescription('Search for an issue in Jira')
+            ->addArgument('search', InputArgument::REQUIRED, 'Search term');
+    }
 
-	public function execute(InputInterface $input, OutputInterface $output) {
-		$api = new Api(
-			getenv('JIRA_URL'),
-			new Basic(getenv('JIRA_USERNAME'), getenv('JIRA_PASSWORD'))
-		);
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $api = new Api(
+            getenv('JIRA_URL'),
+            new Basic(getenv('JIRA_USERNAME'), getenv('JIRA_PASSWORD'))
+        );
 
-		$search = $input->getArgument('search');
+        $search = $input->getArgument('search');
 
-		$walker = new Walker($api);
-		$walker->push("text ~ '{$search}' ORDER BY status ASC");
+        $walker = new Walker($api);
+        $walker->push("text ~ '{$search}' ORDER BY status ASC");
 
-		$rows = $this->buildRows($walker);
+        $rows = $this->buildRows($walker);
 
-		$table = new Table($output);
-		$table->setHeaders(['Key', 'Summary', 'Status', 'Link'])
-			->setRows($rows)
-			->render();
-	}
+        $table = new Table($output);
+        $table->setHeaders(['Key', 'Summary', 'Status', 'Link'])
+            ->setRows($rows)
+            ->render();
+    }
 
-	/**
-	 * Iterate through issues and build array for table structure
-	 *
-	 * @param $walker
-	 *
-	 * @return array
-	 */
-	private function buildRows($walker) {
-		$rows = [];
-		foreach ($walker as $issue) {
-			$rows[] = $this->buildRow($issue);
-		}
+    /**
+     * Iterate through issues and build array for table structure
+     *
+     * @param $walker
+     *
+     * @return array
+     */
+    private function buildRows($walker)
+    {
+        $rows = [];
+        foreach ($walker as $issue) {
+            $rows[] = $this->buildRow($issue);
+        }
 
-		return $rows;
-	}
+        return $rows;
+    }
 
-	/**
-	 * Format individual row for table structure
-	 *
-	 * @param $issue
-	 *
-	 * @return array
-	 */
-	private function buildRow($issue) {
-		return [
-			"<info>{$issue->getKey()}</info>",
-			$issue->getSummary(),
-			$issue->getStatus()['name'],
-			getenv('JIRA_URL') . '/browse/' . $issue->getKey()
-		];
-	}
+    /**
+     * Format individual row for table structure
+     *
+     * @param $issue
+     *
+     * @return array
+     */
+    private function buildRow($issue)
+    {
+        return [
+            "<info>{$issue->getKey()}</info>",
+            $issue->getSummary(),
+            $issue->getStatus()['name'],
+            getenv('JIRA_URL') . '/browse/' . $issue->getKey()
+        ];
+    }
 
 }
